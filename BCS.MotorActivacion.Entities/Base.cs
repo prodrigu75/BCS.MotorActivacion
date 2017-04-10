76287aba9556c410100ba38c7eb8a9592192b36d
prodrigu75 <pablo.rodriguez@entersoft.cl>
2017-04-10 16:09:56 -0300

    201704101609

---
+++ BCS.MotorActivacion.Entities/Base.cs
@@ -0,0 +1,15 @@
+﻿namespace BCS.MotorActivacion.Entities
+{
+    public abstract class Base
+    {
+        #region Member Variables
+        public string mEstado;
+        #endregion
+
+        public string Estado
+        {
+            get { return mEstado; }
+            set { mEstado = value; }
+        }
+    }
+}
---
+++ BCS.MotorActivacion.Entities/Ramos.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace BCS.MotorActivacion.Entities
+{
+    public class Ramos: List<Ramo>
+    {
+    }
+
+    public class Ramo: Base
+    {
+        public short mCodigoRamo;
+        public string mNombre;
+        public string mDescripcion;
+
+        public short CodigoRamo
+        {
+            get { return mCodigoRamo; }
+            set { mCodigoRamo = value; }
+        }
+
+        public string Nombre
+        {
+            get { return mNombre; }
+            set { mNombre = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return mDescripcion; }
+            set { mDescripcion = value; }
+        }
+    }
+}
